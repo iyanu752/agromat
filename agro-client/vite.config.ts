81161c5e2528a7faf693f47ad735bb31bc90cf59
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-05-11 09:25:40 +0100

    takeback

--- agro-client/vite.config.ts
+++ agro-client/vite.config.ts
@@ -1,13 +1,14 @@
 import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/", // ensure this is set for correct routing on Vercel
 })
